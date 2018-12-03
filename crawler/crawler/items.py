d1a8e0d4492b3b6e6ba9fe1c7cdcca76ea743b30
abbas <abbas.hoseini74@gmail.com>
2018-12-03 15:56:15 +0330

    simple crawler

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class NlpcrawlerItem(scrapy.Item):
+class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class NlpcrawlerPipeline(object):
+class CrawlerPipeline(object):
     def process_item(self, item, spider):
         return item
