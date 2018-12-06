9e47c94c2e39ce60045431453cec33595fd87fdf
abbas <abbas.hoseini74@gmail.com>
2018-12-06 09:23:35 +0330

    added simple crawler (#10)
    * added simlpe crawler by scrapy
    
    * simple crawler
    
    * simple crawler
    
    * crawler moved to /scripts

---
+++ scripts/crawler/crawler/spiders/hamshahri_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "hamshahri"
+    start_urls = [
+        'http://www.hamshahrionline.ir/'
+    ]
+    allowed_domains=["hamshahrionline.ir"]
+    def parse(self, response):
+        for quote in response.css('p::text').extract():
+            yield {
+                'text': quote
+            }
+        for href in response.css('a::attr(href)').extract():
+            yield response.follow(href, callback=self.parse)
