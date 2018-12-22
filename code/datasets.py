8b45ca5d133d114e1ec040749a5355129466142f
abbas <abbas.hoseini74@gmail.com>
2018-12-22 10:24:12 +0330

    helpers.py moved question.txt removed loadAnalogyDataset changed to load csv format`

--- code/datasets.py
+++ code/datasets.py
@@ -8,18 +8,16 @@ def loadAnalogyDataset(path):
 
     allfiles = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
     datasets=[]
-    print(allfiles)
     for f in allfiles:
         with open (f, "r") as myfile:
             lines = myfile.read().splitlines()
         dataset={"name":f,'rows':[]}
         cat = None
         for l in lines:
-            if l.startswith(":"):
-                cat =l.lower().split()[1]
-            else:
-                words =  l.split()
-                dataset["rows"].append({'words':words,'category':cat})
+            cols =  l.split(",")
+            if(len(cols)!=5):
+                raise Exception("analogy test set corrupted")
+            dataset["rows"].append({'words':cols[1:],'category':cols[0]})
         datasets.append(dataset)
 
     return datasets
